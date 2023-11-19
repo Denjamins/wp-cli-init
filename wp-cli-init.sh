86c3849d10cf7f3672892b2f807b2d152124d682
Denis Surkov <149530158+Denjamins@users.noreply.github.com>
2023-11-19 11:56:37 +0400

    Check of version work wp-cli-init.sh

--- wp-cli-init.sh
+++ wp-cli-init.sh
@@ -5,7 +5,7 @@
 # GitHub: https://github.com/Denjamins/wp-cli-init 
 
 # Current version of the script
-current_version="1.0"
+current_version="1.01"
 
 # Function to check the version
 check_version() {
