9df0fe5d65fac1270d3e91d5391e4b4d55644510
Denis Surkov <149530158+Denjamins@users.noreply.github.com>
2024-09-10 17:59:32 +0400

    Update wp-cli-init.sh
    fix ver number

--- wp-cli-init.sh
+++ wp-cli-init.sh
@@ -5,7 +5,7 @@
 # GitHub: https://github.com/Denjamins/wp-cli-init 
 
 # Current version of the script
-current_version="1.06"
+current_version="1.07"
 
 # Function to check the version
 check_version() {
